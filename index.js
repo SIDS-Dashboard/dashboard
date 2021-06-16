412c6903edfefd48c19df71708659b8c2aea6a5c
Sebastian Hancock <sebastian.hancock@tum.de>
2021-06-16 14:05:05 -0700

    update

--- index.js
+++ index.js
@@ -5,17 +5,15 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import { MapboxStyleDefinition, MapboxStyleSwitcherControl } from "mapbox-gl-style-switcher";
 import "mapbox-gl-style-switcher/styles.css";
 
-//import gjn from './data/allsids.geojson'
-
 import names from './data/sidsNames.json'
 
 import * as d3 from 'd3-fetch';
 import Pbf from 'pbf'
 import geobuf from 'geobuf';
-import _ from 'lodash';
-import bbox from '@turf/bbox'
+import find from 'lodash.find'
+/* import bbox from '@turf/bbox'
 import bboxPolygon from "@turf/bbox-polygon";
-import hexGrid from "@turf/hex-grid";
+import hexGrid from "@turf/hex-grid"; */
 
 import './style.css'
 //import fetch from "node-fetch";
@@ -33,10 +31,6 @@ const map = new mapboxgl.Map({
   center: [-172.14, -13.79], // starting position [lng, lat]
   zoom: 3, // starting zoom
 });
-
-
-
-//console.log(names);//
 
 //const admin1Regions = 'https://sebastian-ch.github.io/sidsDataTest/data/gadm1.pbf';
 
@@ -67,29 +61,39 @@ map.on('load', function() {
     console.dir(event.target.id);
 
 
-    if(event.target.id !== 'EEZ') {
-      var currbb = _.find(names, ['GID_0', event.target.id ])
-      var v2 = new mapboxgl.LngLatBounds([currbb.bb[0], currbb.bb[1]])
-      //map.fitBounds(getAndFixBbox(), {
-        console.log(v2)
-        map.fitBounds(v2, {
-        linear: true,
-        padding: {top: 10, bottom:25, left: 15, right: 5}
-      });
-
-      addSidOutline(currbb.NAME_0);
-    }
+    //if(event.target.id !== 'EEZ' && event.target.id !== 'hex' && event.target.id !== 'hex1' && event.target.id !== 'pop') 
 
     if (event.target.id === 'EEZ') {
       allEez();
       
     }
 
-   /* if(event.target.id ==='admin') {
-      d3.buffer(admin1Regions).then(function(data) {
-        addAdminToMap(geobuf.decode(new Pbf(data)))
-      })
-    } */
+    if (event.target.id === 'hex') {
+      addHex(event.target.id);
+      
+    }
+
+    if (event.target.id === 'hex1') {
+
+      addHex(event.target.id)
+
+    } if (event.target.id === 'pop') {
+
+      addHex(event.target.id)
+
+    } else {
+      {
+
+        var currbb = find(names, ['GID_0', event.target.id ])
+        var v2 = new mapboxgl.LngLatBounds([currbb.bb[0], currbb.bb[1]])
+          map.fitBounds(v2, {
+          linear: true,
+          padding: {top: 10, bottom:25, left: 15, right: 5}
+        });
+  
+        addSidOutline(currbb.NAME_0);
+      }
+    }
 
   }) 
 
@@ -110,115 +114,169 @@ map.on('load', function() {
 
   map.addControl(new MapboxStyleSwitcherControl(styles), 'top-left');
 
-  /*const samoaUrl = 'https://sebastian-ch.github.io/sidsDataTest/data/sa.pbf';
-  const fijiUrl = 'https://sebastian-ch.github.io/sidsDataTest/data/fiji.pbf'; */
-
   const admin1Regions = 'https://sebastian-ch.github.io/sidsDataTest/data/gadm1.pbf';
-  const allSids = 'https://sebastian-ch.github.io/sidsDataTest/data/allSids.pbf';
+  
   const EEZlines = 'https://sebastian-ch.github.io/sidsDataTest/data/eezlines.pbf';
   
-  var files = [
-    allSids,
-    EEZlines
-    
-  ];
-
-  var promises = [];
-
-
-  files.forEach(function(url) {
-    promises.push(d3.buffer(url))
   
-  }) 
-
-  Promise.all(promises).then(function(allData) {
-    
-    var allSids = geobuf.decode(new Pbf(allData[0]));
-    var eezlines_gjn = geobuf.decode(new Pbf(allData[1]));
-    
-    //console.log(fiji)
-    //addToMap(samoa, fiji, saEez, fEez)
-    //addSamoa(samoa)
-    //allSidsToMap(eezlines_gjn)
-
-
-    addSources(allSids, eezlines_gjn);
-
-  })
 })
 
-function addSources(sids, eez) {
-  //console.log(sids);
-  var sidsSourceName = 'allsids-source';
-  var eezSourceName = 'eez-source'
 
-  map.addSource(sidsSourceName, {
-    'type': 'geojson', 
-    'data': sids
-  });
+function addHex(name) {
 
-  map.addSource(eezSourceName, {
-    'type': 'geojson',
-    'data': eez
-  })
+  var style1 = []
 
-if (map.getSource(eezSourceName)) {
-  document.getElementById('loader').remove()
-}
+  if (name === 'hex'){
+
+    style1 =
+      [
+        'interpolate',
+        ['linear'],
+        ['get', 'rando_1'],
+        0,
+        '#f7fcfd',
+        100,
+        '#e0ecf4',
+        300,
+        '#bfd3e6',
+        400,
+        '#8c96c6',
+        500,
+        '#8c6bb1',
+        700,
+        '#6e016b'
+        ]
+
+  } else if (name === 'hex1') {
+
+    style1 = [
+      'match',
+      ['get', 'rando_3'],
+      1, 'red',
+      2, 'blue',
+      3, 'yellow',
+      4, 'orange',
+      5, 'gray', 'green'
+    ]
+
+  } else if (name === 'pop') {
+
+    style1 = [
+      'interpolate',
+      ['linear'],
+      ['get', '_mean'],
+      0,
+      '#eff3ff',
+      553,
+      '#c6dbef',
+      2302,
+      '#9ecae1',
+      5544,
+      '#6baed6',
+      12070,
+      '#3182bd',
+      33737,
+      '#08519c'
+      ]
+
+      
+
+
+  }
+
+  const hexTest = 'https://sebastian-ch.github.io/sidsDataTest/data/hex.pbf';
+
+  if(map.getSource('hex-source')) {
+    map.setPaintProperty('hex', 'fill-color', style1)
+
+  } else {
+    
+    d3.buffer(hexTest).then(function(data){
+
+      var gjn = geobuf.decode(new Pbf(data));
+
+      map.addSource('hex-source', {
+        'type': 'geojson',
+        'data': gjn
+      })
+    
+      map.addLayer({
+        'id': 'hex',
+        'type': 'fill', 
+        'source': 'hex-source',
+        'layout': {
+          'visibility': 'visible'
+          },
+        'paint': {
+          'fill-color': style1,
+            'fill-opacity': 0.75
+            
+            }
+        },
+        
+    );
   
+    })
 
+  }
 }
 
 
 function addSidOutline(name) {
 
-  //console.log(map.getStyle().sources)
-
   if (map.getLayer('outline')) {
-    map.removeLayer('outline');
-  }
+    map.setFilter('outline',['==', 'NAME_0', name])
 
-  map.addLayer({
-    'id': 'outline',
-    'type': 'line', 
-    'source': 'allsids-source',
-    'layout': {
-      'visibility': 'visible'
-      },
-    'filter': ['==', 'NAME_0', name],
-    'paint': {
-        'line-color': 'red',
-        /*'line-opacity': 0.3, */
-        'line-width': 2
-        }
-    },
+  } else {
+
+      const allSids = 'https://sebastian-ch.github.io/sidsDataTest/data/allSids.pbf';
     
-);
+        d3.buffer(allSids).then(function(data){
 
+          var gjn = geobuf.decode(new Pbf(data));
+
+          map.addSource('outline-source', {
+            'type': 'geojson',
+            'data': gjn
+          })
+
+          map.addLayer({
+            'id': 'outline',
+            'type': 'line', 
+            'source': 'outline-source',
+            'layout': {
+              'visibility': 'visible'
+              },
+            'filter': ['==', 'NAME_0', name],
+            'paint': {
+                'line-color': 'red',
+                /*'line-opacity': 0.3, */
+                'line-width': 2
+                }
+            });
+          })
+
+}
 }
 
 
-map.on('idle', function() {
-  console.log('hi')
-  
-
-
-})
-
-
-/*map.on('style.load', function() {
-  addSamoa(x)
-  map.addControl(new mapboxgl.Minimap(), 'bottom-right');
-}) */
-
-
-
 function allEez() {
+
+  const EEZlines = 'https://sebastian-ch.github.io/sidsDataTest/data/eezlines.pbf';
 
   if (map.getLayer('eez-layer')) {
     map.removeLayer('eez-layer');
     map.removeLayer('disputed');
   } else {
+
+    d3.buffer(EEZlines).then(function(data){
+
+      var gjn = geobuf.decode(new Pbf(data));
+
+      map.addSource('eez-source', {
+        'type': 'geojson',
+        'data': gjn
+      })
 
     map.addLayer({
       'id': 'eez-layer',
@@ -278,156 +336,5 @@ function allEez() {
     },
   );
   }
-
+    )}
 }
-
-
-  function addAdminToMap(data) {
-
-    console.log(data);
-    var sourceName = 'adminregions';
-    var layerName = data.name+ '-layer';
-
-    map.addSource(sourceName, {
-      'type': 'geojson', 
-      'data': data
-    });
-
-    map.addLayer({
-      'id': layerName,
-      'type': 'fill', 
-      'source': sourceName,
-      'layout': {
-        'visibility': 'visible'
-        },
-      'paint': {
-          'fill-color': 'orange',
-          'fill-opacity': 0.5,
-          'fill-outline-color': 'black'
-          }
-      },
-      'aeroway-line' 
-  );
-
-
-  }
-
-
-function getAndFixBbox(data) {
-
-  var theBbox = bbox(data);
-  for (var x in theBbox) {
-    if(theBbox[x] > 0) {
-      theBbox[x] = theBbox[x] - 360;
-    };
-
-  }
-
-  return theBbox
-
-}
-
-function addHexToMap(data) {
-  
-  var hexx = hexGrid(data, 5, { units: 'kilometers'})
-
-  map.addSource('hex-source', {
-    'type': 'geojson', 
-    'data': hexx
-  });
-
-  map.addLayer({
-    'id': 'hex-layer',
-    'type': 'fill', 
-    'source': 'hex-source',
-    'layout': {
-      'visibility': 'visible'
-      },
-    'paint': {
-        'fill-color': 'green',
-        'fill-opacity': 0.3
-        }
-    },
-    'samoa-layer' 
-);
-
-
-}
-
-
-
-function addEezToMap(data, name) {
-
-  
-
-  var sourceName = name + '-source';
-  var layerName = name + '-layer';
-
-  if (name === 'samoaEEZ') {
-    var layerBehind = 'samoa-layer'
-    map.fitBounds(bbox(data), {
-      linear: true,
-      padding: {top: 10, bottom:25, left: 15, right: 5}
-    });
-  } else {
-    var layerBehind = 'fiji-layer'
-  }
-
-  map.addSource(sourceName, {
-    'type': 'geojson', 
-    'data': data
-  });
-
-  map.addLayer({
-    'id': layerName,
-    'type': 'fill', 
-    'source': sourceName,
-    'layout': {
-      'visibility': 'visible'
-      },
-    'paint': {
-        'fill-color': 'blue',
-        'fill-opacity': 0.5
-        }
-    },
-    layerBehind
-);
-
-
-
-
-}
-
-
-
-
-
-function addBoundingBoxToMap(data) {
-
-  data = bboxPolygon(data)
-
-  map.addSource('bbox-source', {
-    'type': 'geojson', 
-    'data': data
-  });
-
-  map.addLayer({
-    'id': 'bbox-layer',
-    'type': 'fill', 
-    'source': 'bbox-source',
-    'layout': {
-      'visibility': 'visible'
-      },
-    'paint': {
-        'fill-color': 'red',
-        'fill-opacity': 0.3
-        }
-    },
-    'samoa-layer' 
-);
-
-
-}
-
-
-
