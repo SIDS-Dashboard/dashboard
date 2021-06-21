1f1dedcfae77aa0eb9683be55fafe3c163fc3133
Sebastian Hancock <sebastian.hancock@tum.de>
2021-06-21 15:42:16 -0700

    remove console

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ var pass = document.getElementById('fname')
 
 pass.addEventListener('input', function(e) {
 
-  console.log(pass.value)
+  //console.log(pass.value)
 
   if(pass.value === 'island') {
     document.getElementById('password').remove()
